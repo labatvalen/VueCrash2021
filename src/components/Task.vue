2f356e5efa8281e6d52fe93966edee01571e1eef
labatvalen <v.labat13@gmail.com>
2021-04-21 20:02:20 +0200

    Dépôt de projet dans git distant

---
+++ src/components/Task.vue
@@ -0,0 +1,47 @@
+<template>
+    <!-- Si reminder est vrai, on ajoute la classe reminder, sinon rien
+        et on veut également la classe task
+         Event en double cliquant sur la div
+         emit renvoie au cran en dessus, ici à Tasks.vue-->
+    <div @dblclick="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' : '', 'task']">
+        <h3>
+            {{ task.text }}
+            <i @click="onDelete(task.id)" class="fas fa-times"></i>
+        </h3>
+        <p>{{task.day}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'Task',
+    props: {
+        task: Object
+    },
+    methods: {
+        onDelete(id) {
+            this.$emit('delete-task', id)
+        }
+    }
+}
+</script>
+
+<style scope>
+    .fas {
+        color: red;
+    }
+    .task {
+        background: #f4f4f4;
+        margin: 5px;
+        padding: 10px 20px;
+        cursor: pointer;
+    }
+    .task.reminder {
+        border-left: 5px solid green;
+    }
+    .task h3 {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
